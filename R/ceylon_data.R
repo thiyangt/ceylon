6deb2b22275951143e325f2443b8800d4c907b32
thiyangt <thiyanga.talagala@monash.edu>
2021-03-09 18:09:48 +0530

    updated files

---
+++ R/ceylon_data.R
@@ -0,0 +1,9 @@
+#' Province level shape file data
+#'@description shape file data for province level
+#'\describe{
+#'\item{PROVINCE}{name of the province}
+#'\item{geometry}{Multipolygon}}
+#'@source https://data.humdata.org/dataset/sri-lanka-administrative-levels-0-4-boundaries
+#'@example
+#'data(sf_sl_1)
+"sf_sl_1"
--- data-raw/dataraw.R
+++ data-raw/dataraw.R
@@ -17,11 +17,10 @@ library(sf)
 sf_sl_1 <- read_sf("data-raw/lka_adm_slsd_20200305_shp/lka_admbnda_adm1_slsd_20200305.shp") %>%
   select(ADM1_EN, geometry) # Select the level 1 districts (adm1)
 sf_sl_1 <- sf_sl_1 %>% st_transform(5235)
-
-read.dcf(system.file("DESCRIPTION", package = "ceylon", mustWork = TRUE))
-usethis::use_data(sf_sl_1)
-#sf_sl_1 <- sf_sl_1 %>% 
-#  mutate(PROVINCE = str_to_upper(ADM1_EN)) %>% 
-#  select(-ADM1_EN)
+class(sf_sl_1)
+sf_sl_1 <- sf_sl_1 %>% 
+  mutate(PROVINCE = str_to_upper(ADM1_EN)) %>% 
+  select(-ADM1_EN)
+save(sf_sl_1, file="data/sf_sl_1.rda")
 ggplot(sf_sl_1) + 
   geom_sf() 
