e1980222bf4f4976fd48ffa9180ae9b222c8c17b
thiyangt <thiyanga.talagala@monash.edu>
2021-03-10 07:56:02 +0530

    deleted large files

--- R/ceylon_data_4.R
+++
@@ -1,9 +0,0 @@
-#' Province level shape file data
-#'@description shape file data for province level
-#'\describe{
-#'\item{ADM4_EN}{Divisional secretariat}
-#'\item{geometry}{Multipolygon}}
-#'@source https://data.humdata.org/dataset/sri-lanka-administrative-levels-0-4-boundaries
-#'@example
-#'data(sf_sl_4)
-"sf_sl_4"
